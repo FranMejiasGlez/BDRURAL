76f733b7c426efd17851bd0a80928e2132b86ac4
fran gonzalez <franciscomejias.24@campuscamara.es>
2026-02-08 15:05:47 +0100

    BSCRIPT changes

--- Equipo1Alquiler/src/model/JDBC.java
+++ Equipo1Alquiler/src/model/JDBC.java
@@ -4,9 +4,15 @@
  */
 package model;
 
-import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -18,15 +24,41 @@ public class JDBC {
     private String sentenciaSQL;
     private ResultSet cursor;
 
-    public boolean setConexion(File properties) {
+    public boolean setConexion(Properties properties) {
+        String nombreDriver = "";
+        String urlConexion = "";
+        String usuario = "";
+        String contra = "";
+        try {
+            properties.load(new FileInputStream("src/MySql.properties"));
+            nombreDriver = properties.getProperty("driver");
+            urlConexion = properties.getProperty("url");
+            usuario = properties.getProperty("usuario");
+            contra = properties.getProperty("password");
+        } catch (IOException ex) {
+            System.out.println("Error de E/S con fichero properties");
+        }
+        try {
+            try {
+                Class.forName(nombreDriver);
+            } catch (ClassNotFoundException ex) {
+                System.out.println("Clase no encontrada");
+            }
+            this.conexion = DriverManager.getConnection(urlConexion, usuario, contra);
+            System.out.println("Conexion exitosa");
+        } catch (SQLException ex) {
+            System.out.println("No se pudo realizar la conexion: " + ex.getMessage());
+        }
         return false;
     }
 
     public Connection getConexion() {
-        return null;
+        return this.conexion;
     }
 
     public void setSentenciaSQL(String strSQL) {
+        this.sentenciaSQL = strSQL;
+
     }
 
     public boolean ejecutarConsulta() {
